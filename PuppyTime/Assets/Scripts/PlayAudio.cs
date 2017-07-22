0a5b19ccc1cd3439a0e1e22e183273cd3ef128d2
Trevyn Mace <trevyn@trevynmace.com>
2017-07-22 13:18:33 -0600

    initial commit with assets

---
+++ PuppyTime/Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlayer : MonoBehaviour
+{
+	void Start()
+	{
+		AudioSource audio = GetComponent<AudioSource> ();
+		audio.Play ();
+		audio.Play (44100);
+	}
+}
