0cd6100ccb042269a93a720c0eb8648ede4b0fc3
Trevyn Mace <trevyn@trevynmace.com>
2017-07-22 19:49:01 -0600

    WIP: need to fix the time until the camera moves, need to get dogs to move independent of the others, need to add VR support

---
+++ PuppyTime/Assets/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private DateTime _startTime;
+
+    // Use this for initialization
+    void Start ()
+    {
+	    _startTime = DateTime.Now;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    var currentTime = DateTime.Now;
+	    var elapsedTime = (currentTime - _startTime).TotalSeconds;
+
+        //TODO: this should last longer
+	    if (elapsedTime > 10)
+	    {
+	        //rotate player to dinosaur and have it roar
+	        transform.rotation = Quaternion.Euler(-20, 141, 0);
+	        var trexAnimator = GameObject.Find("t-rex").GetComponent<Animator>();
+	        trexAnimator.SetTrigger("go");
+	    }
+
+    }
+}
---
+++ PuppyTime/Assets/PuppyActionRandomizer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class PuppyActionRandomizer : MonoBehaviour
+{
+    private Animator animator;
+    private DateTime _lastUpdateTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var random = new Random();
+	    var randomInt = random.Next(1, 7);
+        
+	    var currentTime = DateTime.Now;
+	    var timeSinceLastAnimationWasSet = (currentTime - _lastUpdateTime).TotalSeconds;
+	    if (timeSinceLastAnimationWasSet > 1)
+	    {
+	        switch (randomInt)
+	        {
+	            case 1:
+	                SetAnimationBooleans("isPlaying");
+	                break;
+	            case 2:
+	                SetAnimationBooleans("isCrying");
+	                break;
+	            case 3:
+	                SetAnimationBooleans("isSleeping");
+	                break;
+	            case 4:
+	                SetAnimationBooleans("isJumping");
+	                break;
+	            case 5:
+	                SetAnimationBooleans("isSittingHigh");
+	                break;
+	            case 6:
+	                SetAnimationBooleans("isPawing");
+	                break;
+	            default: break;
+	        }
+	    }
+	}
+
+    //TODO: how to get each dog to do one of the 6 animations independently?
+    private void SetAnimationBooleans(string boolToSetTrue)
+    {
+        _lastUpdateTime = DateTime.Now;
+
+        animator.SetBool("isPlaying", false);
+        animator.SetBool("isCrying", false);
+        animator.SetBool("isSleeping", false);
+        animator.SetBool("isJumping", false);
+        animator.SetBool("isSittingHigh", false);
+        animator.SetBool("isPawing", false);
+
+        animator.SetBool(boolToSetTrue, true);
+    }
+}
